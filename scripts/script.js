9e41c9b576cda92e4340a2e4f86ee57fefca0e31
rashik <rkoirala43@gmail.com>
2024-09-26 10:28:38 +0545

    Add animation classes and variables & code refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,15 @@
 const taskInput = document.querySelector(".task-input");
 const taskAddBtn = document.querySelector(".task-add-btn");
 const taskList = document.querySelector(".task-list");
-const taskArray = [];
 
-taskAddBtn.addEventListener("click", () => {
+const taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+
+//animation variables
+const fadeOut = 500;
+const greyOut = 500;
+
+//add task to the array
+const addTask = () => {
   const task = taskInput.value;
   if (task) {
     taskArray.push({
@@ -14,17 +20,24 @@ taskAddBtn.addEventListener("click", () => {
   }
   taskInput.value = "";
   sortTasks();
-  renderTasks();
-});
+  renderTasks(true);
+};
 
-const renderTasks = () => {
+//render tasks from the array
+const renderTasks = (addAnimation = false) => {
+  //add to local storage
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
+
+  //clear task list
   taskList.innerHTML = "";
-  taskArray.forEach((task) => {
+  taskArray.forEach((task, i) => {
     // create elements
     const li = document.createElement("li");
+    const wrapper = document.createElement("div");
     const checkbox = document.createElement("input");
     const deleteIcon = document.createElement("img");
     const text = document.createElement("span");
+    const date = document.createElement("span");
 
     //delete icon attributes
     deleteIcon.classList.add("delete-icon");
@@ -32,16 +45,21 @@ const renderTasks = () => {
     deleteIcon.addEventListener("click", deleteTask);
 
     //checkbox attributes
+    checkbox.classList.add("checkbox");
     checkbox.type = "checkbox";
     checkbox.addEventListener("click", toggleDone);
 
-    //append
-    li.appendChild(checkbox);
-    li.appendChild(text);
-    li.appendChild(deleteIcon);
-
     // list style
+    wrapper.classList.add("task-wrapper");
+    date.classList.add("hide");
     li.classList.add("task-item");
+
+    //animation for when a task is added
+    if (i === 0 && addAnimation) {
+      li.classList.add("fade-in");
+    } else {
+      li.classList.remove("fade-in");
+    }
 
     //check if task is done
     if (task.isDone) {
@@ -51,12 +69,19 @@ const renderTasks = () => {
 
     //add the task name
     text.textContent = task.taskName;
+    date.textContent = task.createdAt;
 
-    //append to list
+    //append
+    wrapper.appendChild(checkbox);
+    wrapper.appendChild(text);
+    li.appendChild(wrapper);
+    li.appendChild(deleteIcon);
+    li.appendChild(date);
     taskList.appendChild(li);
   });
 };
 
+//sort the tasks based on date and done status
 const sortTasks = () => {
   taskArray.sort((a, b) => {
     if (a.isDone === b.isDone) {
@@ -68,22 +93,65 @@ const sortTasks = () => {
 };
 
 const deleteTask = (event) => {
-  const taskName = event.target.previousElementSibling.textContent;
-  taskArray.forEach((task, index) => {
-    if (task.taskName === taskName) {
-      taskArray.splice(index, 1);
-    }
-  });
-  renderTasks();
+  const taskElement = event.target.parentElement;
+  const date = taskElement.lastChild.textContent;
+
+  //check and remove fade-in class to not conflict with animation
+  if (taskElement.classList.contains("fade-in")) {
+    taskElement.classList.remove("fade-in");
+  }
+
+  // Apply fade-out class first
+  taskElement.classList.add("fade-out");
+
+  // Wait for animation to finish, then remove the task
+  setTimeout(() => {
+    taskArray.forEach((task, index) => {
+      if (task.createdAt.toString() === date) {
+        taskArray.splice(index, 1);
+      }
+    });
+    renderTasks();
+  }, fadeOut);
 };
 
 const toggleDone = (event) => {
-  const text = event.target.nextElementSibling.textContent;
-  taskArray.forEach((task) => {
-    if (task.taskName === text) {
-      task.isDone = !task.isDone;
-    }
-  });
-  sortTasks();
-  renderTasks();
+  const taskElement = event.target.parentElement.parentElement;
+  const date = taskElement.lastChild.textContent;
+
+  //check and remove fade-in class to not conflict with animation
+  if (taskElement.classList.contains("fade-in")) {
+    taskElement.classList.remove("fade-in");
+  }
+
+  // toggling grey in and out animations
+  if (taskElement.classList.contains("done")) {
+    taskElement.classList.remove("done");
+    taskElement.classList.add("grey-in");
+  } else {
+    taskElement.classList.add("grey-out");
+  }
+
+  // Wait for animation to finish, then toggle the task
+  setTimeout(() => {
+    taskArray.forEach((task) => {
+      if (task.createdAt.toString() === date) {
+        task.isDone = !task.isDone;
+      }
+    });
+    sortTasks();
+    renderTasks();
+  }, greyOut);
 };
+
+//event listeners
+taskAddBtn.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+//initial render
+renderTasks();
